c91c93a2ec045ff1bf6368d288f925ff9d2b5ee2
andrey <anvamp.klcomputers@gmail.com>
2015-11-13 00:11:29 +0300

    Fix padding on events list items

--- views/pages/news-and-events.php
+++ views/pages/news-and-events.php
@@ -5,7 +5,7 @@
             <ul id="lessonsList">
                 <li><a href="/marianna-high-achiever-award">Anastasiya's student receives High Achiever Award - November 2015</a></li>
                 <li><a href="/julian-callan-associate-teacher-diploma">Anastasiya's student receives the Associate Diploma of RIAM - July 2015</a></li>
-                <li><a href="/student-summer-recital-june-2015">Students Summer Recital &mdash; June 2015</a></li>
+                <li><a href="/student-summer-recital-june-2015">Students Summer Recital - June 2015</a></li>
                 <li><a href="/christmas-recital-2014">Christmas Recital 2014</a></li>
             </ul>
             <div class="bodytext">
