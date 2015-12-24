c7a9367aa5c38a44497cd4d25ee96761f14b6f12
andrey <aglushkov@shakuro.com>
2015-12-24 22:54:50 +0300

    Add new event page

---
+++ views/pages/events/laura-dowling-associate-teacher-diploma.php
@@ -0,0 +1,11 @@
+<div id="mainimg">
+    <div id="header" style="margin-top:10px;">
+        <h1>Anastasiya's student receives the Associate Diploma of RIAM - July 2015</h1>
+
+        <div class="bodytext">
+            <p>Laura Dowling, a student of Anastasiya, was granted the Associate Teacher Diploma (ARIAM) of Royal Irish Academy of Music.</p>
+            <p>A big congratulations to Laura for this wonderful achievement!</p>
+            <img style="display: block; margin: 0 auto;"  src='/images/student_laura_granted_diploma_by_riam.jpg'></img>
+        </div>
+    </div>
+</div>
